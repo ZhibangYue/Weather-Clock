342a25bfeab205fd82540094d6270a4da9fc240a
yuezhibang <yuezhibang@126.com>
2024-04-21 01:03:38 +0800

    0.1.0

---
+++ Info.h
@@ -0,0 +1,15 @@
+#ifndef INFO_H_
+#define INFO_H_
+
+#include <Arduino.h>
+
+class Info {
+  public:
+    // 提醒相关
+    static const char* reminder_host;
+    static String reqUserKey;  // 私钥
+  private:
+    
+};//注意这边要有分号结束
+
+#endif
